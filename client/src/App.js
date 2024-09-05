a4227608e9ed1a5662a0df5e9da08b3088d0ec58
Daniel Kim <daniel.j.kim164@gmail.com>
2024-09-05 01:23:12 -0400

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-const url = process.env.NODE_ENV === 'production' ? 'https://course-tools-demo.onrender.com/' : 'http://127.0.0.1:8000/';
+const url = "https://hai-interaction.onrender.com/"
 function App() {
   const [message, setMessage] = useState("");
   const [response, setResponse] = useState("No response yet");
@@ -24,18 +24,28 @@ function App() {
     setMessage(e.target.value); 
   }
   return (
-    <div className="container mx-auto mt-10">
-      <h1 className="text-4xl">Ask Anything!</h1>
+    <div className="h-screen w-full flex flex-col p-4">
+      <h1 className="text-4xl ml-2 mb-4">DogGPT</h1>
+        <div className="bg-white shadow-lg rounded-lg p-4 h-full overflow-y-auto border border-gray-300">
+          <div className="chat chat-start">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Apollo"
+                  src="https://i.ibb.co/gzZKJjt/apollo.jpg" />
+              </div>
+            </div>
+            <div className="chat-header">
+              Apollo
+            </div>
+            <div className="chat-bubble chat-bubble-warning">Woof woof! Hi Joy! I love you! I miss you!</div>
+          </div>
+      </div>
       <div className="mt-5 flex gap-2">
-        <input type="text" placeholder="Type your message here" value={message} className="input input-bordered w-full max-w-xs" onInput={handleMessage} />
+        <input type="text" placeholder="Type your message here" value={message} className="input input-success w-full max-w-x" onInput={handleMessage} />
         <button className="btn" onClick={sendMessage}>Send</button>
       </div>
-      <div className="card mt-10">
-        <h2 className="text-xl">Response</h2>
-        <div className="mt-5">
-        🤖: {response}
-        </div>
-      </div>
+      
     </div>
   );
 }
